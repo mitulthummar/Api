c00349ae45b3d765df786b1eef4479e7052f48ad
Mitul Thummar <121297061+mitulthummar@users.noreply.github.com>
2023-04-09 11:04:17 +0530

    Add files via upload

---
+++ IUser.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+    Users_id ?: number;
+    name: string;
+    email: string;
+    address1: string;
+    phone: string;
+    street: string;
+    city: string;
+    password:string;
+    state: string;
+    pincode: string;
+  }
+  
